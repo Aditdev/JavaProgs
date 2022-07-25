bed76a59782a88c63cb10fc83296d1f751cc0de8
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-07-25 11:07:09 +0530

    First program.

---
+++ first_prog.java
@@ -0,0 +1,10 @@
+class Tester {
+	public static void main(String[] args) {
+	    int start =1, pval=8,i;
+	    for (i=0;i<pval ;i++){
+	        System.out.print(start + " ");
+	        start *= 2;
+	    } 
+	    
+	}
+}
