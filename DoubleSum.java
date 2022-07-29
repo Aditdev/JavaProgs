4f3f363a6e000f85f5383592e32ce42c3f1df91d
Aditya Lawate <92054021+Aditdev@users.noreply.github.com>
2022-07-29 10:16:11 +0530

    Double sum

---
+++ DoubleSum.java
@@ -0,0 +1,19 @@
+/*
+This Prints 1+1/2+1/3+1/4+.....1/n
+*/
+import java.util.*;
+
+class DoubleSum {
+
+    public static double findHPSum(int num) {
+        if(num>1) return((double)1/num +findHPSum(num-1));
+    return num;
+        
+    }
+    
+    public static void main(String args[]) {
+        Scanner scan = new Scanner(System.in);
+        n = scan.nextInt();
+        System.out.println(findHPSum(n));
+    }
+}
